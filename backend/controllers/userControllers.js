8ea4f476f03d5fc75fcfcd8ce8f0438d801202cb
Arish82 <arish123.ahamad@gmail.com>
2023-12-19 08:50:06 +0530

    generated tokens on signup

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,33 +1,37 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
+const generateWebToken = require('../config/generateToken');
 
 const registerUser = asyncHandler( async (req,res)=>{
     const {name, email, password, pic} = req.body;
-
+    // checking all required fields
     if(!name || !email || !password) {
-        res.status(400).json({message: 'Please enter all feilds', status: 400});
+        res.status(422).json({message: 'Please enter all feilds', status: 422});
         throw new Error('Please enter all feilds')
     }
 
+    // check for existing user
     const userExist = await User.findOne({email});
 
     if(userExist) {
-        res.status(400).json({message: "User already exist", status : 400})
+        res.status(422).json({message: "User already exist", status : 422})
             throw new Error("User already Exists");
     }
     
+    // creating a user
     const user = await User.create({
         name,
         email,
         password,
-        pic
+        pic,
     })
-
+    
     if(user) {
         res.status(201).json({
             message: 'User created successfully',
             email: user.email,
-            name: user.name
+            name: user.name,
+            token: generateWebToken(user._id)
         });
     }
     
