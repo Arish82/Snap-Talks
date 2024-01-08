22682e256e1e33acb533ad8325e4931be28f4d54
Arish82 <arish123.ahamad@gmail.com>
2024-01-08 16:00:21 +0530

    socket io in backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const app=express();
+var io = require("socket.io")
 
 // dotenv setup
 const dotenv=require("dotenv");
@@ -27,10 +28,44 @@ app.get("/",(req,res)=>{
 })
 
 // server is created
-app.listen(PORT, (err)=>{
+const server = app.listen(PORT, (err)=>{
     if(err) {
         console.log(`Server failed to response, ${err}`);
         return;
     }
     console.log(`Server is running on PORT, ${PORT}`)
 } )
+
+
+io =io(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000"
+    },
+})
+
+io.on("connection", (socket)=>{
+    console.log("connected to socket.io");
+    socket.on('setup', (userData)=>{
+        socket.join(userData._id);
+        console.log(userData._id);
+        socket.emit("connected");
+    })
+
+    socket.on("join chat", (room)=>{
+        socket.join(room);
+        console.log("User Joined Room: "+ room);
+    });
+
+    socket.on("new message", (newMessageReceived)=>{
+        var chat=newMessageReceived.chat;
+
+        if(!chat.users) return console.log("chat.users not defined in the chat");
+
+        chat.users.forEach(user =>{
+            if(user._id == newMessageReceived.sender._id) return;
+
+            socket.in(user._id).emit("message received", newMessageReceived);
+        })
+    })
+})
